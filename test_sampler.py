fdffdf0ac8f664ffa1e509d98e88888110b5cace
iltya <iltyakov.nik@mail.ru>
2023-07-05 14:49:45 +0300

    fix calculating grads in test_sampler

--- test_sampler.py
+++ test_sampler.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import copy
 
 class TestProblem:
     CONST = 0.5
@@ -12,24 +13,29 @@ class TestProblem:
 
         self.x = torch.tensor(np.random.random((self.x_dim)), requires_grad=True).float()
         self.y = torch.tensor(np.random.random((self.y_dim)), requires_grad=True).float()
-
-
-    def calc(self):
-        x_y = torch.concatenate([self.x, self.y], dim=0)
-        b = torch.Tensor(np.random.normal(size=(self.x_dim + self.y_dim, self.x_dim + self.y_dim)))
-        a_matrix = b * torch.transpose(b, 0, 1)
-        b_vector = torch.Tensor(np.random.random(self.m))
-        c_matrix = torch.Tensor(np.random.random((self.m, self.x_dim)))
-
-        summand_1 = a_matrix @ x_y @ x_y * self.CONST
-        summand_2 = self.gamma * torch.logsumexp((c_matrix @ self.x - b_vector) / self.gamma, dim=0)
-        result = summand_1 + summand_2
+        self.b = torch.Tensor(np.random.normal(size=(self.x_dim + self.y_dim, self.x_dim + self.y_dim)))
+        self.b_vector = torch.Tensor(np.random.random(self.m))
+        self.c_matrix = torch.Tensor(np.random.random((self.m, self.x_dim)))
 
         self.x.retain_grad()
         self.y.retain_grad()
+
+    def calc(self):
+
+        x_y = torch.concatenate([self.x, self.y], dim=0)
+        a_matrix = b * torch.transpose(b, 0, 1)
+
+        summand_1 = a_matrix @ x_y @ x_y * self.CONST
+        summand_2 = self.gamma * torch.logsumexp((self.c_matrix @ self.x - self.b_vector) / self.gamma, dim=0)
+        result = summand_1 + summand_2
+
         result.backward()
 
-        x_grad = self.x.grad
-        y_grad = self.y.grad
+        x_grad = copy.deepcopy(self.x.grad)
+        y_grad = copy.deepcopy(self.y.grad)
+
+        self.x.grad.zero_()
+        self.y.grad.zero_()
+
 
         return result, x_grad, y_grad
