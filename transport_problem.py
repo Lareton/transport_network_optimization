978e6b7be81f6b002c51be7996dcda0b006f0a83
krammnic <75219117+krammnic@users.noreply.github.com>
2023-07-10 14:21:41 +0300

    new oracle + major refactor

--- transport_problem.py
+++ transport_problem.py
@@ -1,77 +1,108 @@
+import numba
 import numpy as np
 from dataclasses import dataclass
 from typing import TypeVar
 
-T = TypeVar()
-I = TypeVar()
+import scipy
+from graph_tool.topology import shortest_distance
+
+from numba import njit
+from numba.core import types
 
 
 @dataclass
 class Params:
-    T: float | I
-    lambda_: T
-    mu: T
-    gamma: float | I
-    l: float | I
+    gamma: float
+    l: float
     w: float
-    f_flow: float | I
+    f_flow: float
     mu_pow: float
-    t_flow: float | I
-    raw: float | I
-    mu_pow: float | I
+    t_flow: float
+    rho: float
+    mu_pow: float
 
 
-class Grad:
+class DualOracle:
 
-    def __init__(self, params):
+    def __init__(self, graph, net_df, corrs, la, mu, params):
         self.params = params
+        self.graph = graph
+        self.net_df = net_df
+        self.corrs = corrs
+        edges_arr = graph.get_edges()
+        self.edge_to_ind = numba.typed.Dict.empty(key_type=types.UniTuple(types.int64, 2),
+                                                  value_type=numba.core.types.int64)
+        for i, edge in enumerate(edges_arr):
+            self.edge_to_ind[tuple(edge)] = i
 
-    def grad_T(self):
-        ...
+        self.zones_num = corrs.shape[0]
+        self.edges_num = len(graph.ep.capacity.a)
+        self.nodes_cnt = self.zones_num
+        self.edge_cnt = self.edges_num
+
+        self.f_bar = np.array(net_df['capacity'])
+        self.t_bar = np.array(net_df['free_flow_time'])
+        self.t = self.t_bar.copy() + np.random.rand(self.edge_cnt)
+
+        self.la = la
+        self.mu = mu
+
+    @njit
+    def sum_flows_from_tree(self, flows, source, targets, pred_map_arr, corrs, edge_to_ind):
+        for v in targets:
+            corr = corrs[source, v]
+            while v != source:
+                v_pred = pred_map_arr[v]
+                flows[edge_to_ind[(v_pred, v)]] += corr
+                v = v_pred
+        return flows
+
+    def calc_F(self, T):
+        return self.params.gamma * scipy.special.logsumexp(
+            (-T + self.la[..., None] + self.mu[
+                None, ...]) / self.params.gamma) - self.params.l @ self.la - self.params.w @ self.mu + np.sum(
+            self.sigma_star(self.t, self.t_bar, self.params.mu_pow, self.params.rho))
+
+    def d(self, T):
+        return np.exp((-T + self.la + self.mu) / self.params.gamma) / np.sum(
+            np.exp((-T + self.la + self.mu) / self.params.gamma))
 
     def invert_tau(self, t):
-        return (
-                self.params.f_flow * t ** self.params.mu_pow - self.params.f_flow - self.params.t_flow ** self.params.mu_pow) / (
-                self.params.t_flow ** self.params.mu_pow * self.params.raw)
+        return self.f_bar * ((t - self.t_bar) / (self.params.k * self.t_bar)) ** self.params.mu_pow
 
-    def d(self):
-        return np.exp((-self.params.T + self.params.lambda_ + self.params.mu) / self.params.gamma) / np.sum(
-            np.exp((-self.params.T + self.params.lambda_ + self.params.mu) / self.params.gamma))
+    def grad_dF_dt(self, grad_t):
+        return grad_t + self.invert_tau(self.t)
 
-    def grad_dF_dLambda(self):
-        return np.sum(np.exp(-self.params.T + self.params.lambda_[None, ...] + self.params.mu[..., None]),
-                      axis=1) / np.sum(
-            np.exp(-self.params.T + self.params.lambda_[None, ...] + self.params.mu[..., None])) + self.params.l
+    def grad_dF_dla(self, T):
+        return np.sum(np.exp(-T + self.la[..., None] + self.mu[None, ...]), axis=1) / np.sum(
+            np.exp(-T + self.la[..., None] + self.mu[None, ...])) - self.params.l
 
-    def grad_dF_dt(self):
-        return self.params.d() * self.params.grad_T() + self.invert_tau(self.params.T)
+    def grad_dF_dmu(self, T):
+        return np.sum(np.exp(-T + self.la[..., None] + self.mu[None, ...]), axis=0) / np.sum(
+            np.exp(-T + self.la[..., None] + self.mu[None, ...])) - self.params.w
 
-    def grad_dF_dmu(self):
-        return np.sum(np.exp(-self.params.T + self.params.lambda_[None, ...] + self.params.mu[..., None]),
-                      axis=0) / np.sum(
-            np.exp(-self.params.T + self.params.lambda_[None, ...] + self.params.mu[..., None])) + self.params.w
+    def get_T_and_grad_t(self, g, sources, targets):
+        """
+        Считает градиент F по t = суммарные потоки по ребра при прохождениии из всех истоков во все стоки
+        """
 
+        flows_on_shortest = np.zeros(self.edges_num)
+        T = np.zeros((len(sources), len(targets)))
 
-class TransportProblem:
-    def __init__(self, params):
-        self.params = params
+        for source in sources:
+            short_distances, pred_map = shortest_distance(g, source=source, target=targets, weights=g.ep.fft,
+                                                          pred_map=True)
 
-    def sigma_star(self, t):
-        return self.params.f_flow * (
-                (t - self.params.t_flow) / (self.params.t_flow * self.params.raw)) ** self.params.mu_pow * (
-                t - self.params.t_flow) / (1 + self.params.mu_pow)
+            for j in range(len(short_distances)):
+                T[source, j] = short_distances[j]
 
-    def log_sum(self) -> float:
-        return np.log(
-            np.sum(np.exp((np.sum(-self.params.T + self.params.lambda_[None, ...],
-                                  axis=1) + self.params.mu) / self.params.gamma)))
+            self.sum_flows_from_tree(flows_on_shortest, source, targets, np.array(pred_map.a), self.corrs,
+                                     self.edge_to_ind)
 
-    def sigma_star_sum(self) -> np.array:
-        return np.sum(self.sigma_star(self.params.t))
+        return T, flows_on_shortest
 
-    def calc(self) -> float:
-        return self.params.gamma * self.log_sum() - (self.params.l @ self.params.lambda_) - (
-                self.params.w @ self.params.mu) + self.sigma_star_sum()
+    def sigma_star(self, t, t_bar, mu_pow, rho):
+        return self.f_bar * ((t - t_bar) / (t_bar * rho)) ** mu_pow * (t - t_bar) / (1 + mu_pow)
 
 
 def main():
